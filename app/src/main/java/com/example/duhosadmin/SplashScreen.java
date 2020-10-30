d6c5b45429d8feea6bd7ff8d9b6b3f24b3880049
Kreso <kresimirtomic1998@mail.com>
2020-10-31 00:25:10 +0100

    Commmit message

--- app/src/main/java/com/example/duhosadmin/SplashScreen.java
+++ app/src/main/java/com/example/duhosadmin/SplashScreen.java
@@ -1,5 +1,8 @@
 package com.example.duhosadmin;
 
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -9,6 +12,7 @@ import android.view.WindowManager;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
 import java.util.Objects;
 
 public class SplashScreen extends AppCompatActivity {
@@ -31,5 +35,33 @@ public class SplashScreen extends AppCompatActivity {
                 SplashScreen.this.finish();
             }
         },3600);
+    }
+    public boolean isApplicationSentToBackground(final Context context)
+    {
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
+        if (!tasks.isEmpty()) {
+            ComponentName topActivity = tasks.get(0).topActivity;
+            if (!topActivity.getPackageName().equals(context.getPackageName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+    @Override
+    public void onStop() {
+        if (isApplicationSentToBackground(this)){
+            //put your code here what u want to do
+            finish();
+            System.exit(0);
+        }
+        super.onStop();
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        System.exit(0);
+        super.onBackPressed();
     }
 }
